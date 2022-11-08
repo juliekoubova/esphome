6ab1b3db381db26650c87502a8067c2fb62a7c8a
Julie Koubova <julie@koubova.net>
2022-11-08 21:11:46 +0000

    ;

--- esphome/components/jablotron_pg/switch/pg_switch.cpp
+++ esphome/components/jablotron_pg/switch/pg_switch.cpp
@@ -7,7 +7,7 @@ void PGSwitch::register_parent(jablotron::JablotronComponent &parent) { parent.r
 
 void PGSwitch::write_state(bool state) {
   std::string command = state ? "PGON " : "PGOFF ";
-  this->parent_->queue_request_access_code(command + this->get_index_string())
+  this->parent_->queue_request_access_code(command + this->get_index_string());
 }
 
 void PGSwitch::set_state(bool state) { this->publish_state(state); }
